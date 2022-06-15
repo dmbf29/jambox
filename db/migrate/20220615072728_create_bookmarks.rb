1098e012a7094b204f3570fa0b18ec6a27c8fb05
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-15 16:34:32 +0900

    updated migration for votable

--- db/migrate/20220615072728_create_bookmarks.rb
+++ db/migrate/20220615072728_create_bookmarks.rb
@@ -1,7 +1,6 @@
 class CreateBookmarks < ActiveRecord::Migration[6.1]
   def change
     create_table :bookmarks do |t|
-      t.integer :cached_votes_total, default: 0
       t.references :user, null: false, foreign_key: true
       t.references :album, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_073146) do
+ActiveRecord::Schema.define(version: 2022_06_15_073329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,11 +31,17 @@ ActiveRecord::Schema.define(version: 2022_06_15_073146) do
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer "cached_votes_total", default: 0
     t.bigint "user_id", null: false
     t.bigint "album_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
     t.index ["album_id"], name: "index_bookmarks_on_album_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
