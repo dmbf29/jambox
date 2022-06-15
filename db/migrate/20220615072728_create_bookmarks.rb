57620d116535657b6182cca012e629a0a5463139
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-15 16:27:37 +0900

    Added models

---
+++ db/migrate/20220615072728_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookmarks do |t|
+      t.integer :cached_votes_total
+      t.references :user, null: false, foreign_key: true
+      t.references :album, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
