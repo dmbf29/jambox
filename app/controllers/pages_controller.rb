49a00fa7f1cd8ae1c9dce203881947781a54fe84
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-16 12:00:18 +0900

    added bookmarks controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :check_signed_in
 
   def home
+  end
+
+  private
+
+  def check_signed_in
+    redirect_to bookings_path if signed_in?
   end
 end
