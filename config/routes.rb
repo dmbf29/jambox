33335e0e681082cd58959978331d3a2444d7867b
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-17 17:05:02 +0900

    navbar top completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :bookmarks, only: [:index]
+  resources :bookmarks, only: [:index, :new]
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
