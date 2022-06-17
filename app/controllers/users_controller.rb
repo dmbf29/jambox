d149d4fd8d6cdc45cf6d8259d7fc121e4747db73
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-17 12:13:43 +0900

    completed spotify log in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
     if user.persisted?
       remember_me(user)
       sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'Spotify') if is_navigational_format?
     else
-      session['devise.facebook_data'] = request.env['omniauth.auth']
+      session['devise.spotify_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
