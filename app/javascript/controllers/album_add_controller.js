ad9f07504585a7a9e3cd0d05e1129970dde95790
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-19 14:12:56 +0900

    toggle and untoggle album chosen

--- app/javascript/controllers/album_add_controller.js
+++ app/javascript/controllers/album_add_controller.js
@@ -1,13 +1,20 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ["form", "name", "spotify_id", "release_date", "card"];
 
   connect() {
     console.log("connected");
   }
 
   fillForm(event) {
+    this.cardTargets.forEach((card) => {
+      card.classList.remove("border-orange");
+    });
+    event.currentTarget.classList.add("border-orange");
     this.formTarget.classList.remove("d-none");
+    this.nameTarget.value = event.currentTarget.dataset.name;
+    this.spotify_idTarget.value = event.currentTarget.dataset.spotifyId;
+    this.release_dateTarget.value = event.currentTarget.dataset.releaseDate;
   }
 }
